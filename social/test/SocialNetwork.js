9ebbbd099be1a470a489456eef0277c356055a53
Danielle Brinkley <daniellebrinkley.fs@gmail.com>
2022-03-08 03:37:15 -0700

    install dependencies

---
+++ social/test/SocialNetwork.js
@@ -0,0 +1,21 @@
+const SocialNetwork = artifacts.require('./SocialNetwork.sol');
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('SocialNetwork', (accounts) => {
+    let socialNetwork;
+
+        describe('deployment', async () => {
+            it('deploys successfully', async () => {
+                socialNetwork = await SocialNetwork.deployed()
+                const address = await socialNetwork.address
+                assert.notEqual(address, 0x0)
+                assert.notEqual(address, '')
+                assert.notEqual(address, null)
+                assert.notEqual(address, undefined)
+            })
+        })
+    
+})
