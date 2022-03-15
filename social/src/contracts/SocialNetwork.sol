02ae5c97ff682a2c04b0ec2ad801eaabadb7c3f2
Danielle Brinkley <daniellebrinkley.fs@gmail.com>
2022-03-15 03:57:11 -0600

    update test to create posts list posts and tip posts

--- social/src/contracts/SocialNetwork.sol
+++ social/src/contracts/SocialNetwork.sol
@@ -2,9 +2,24 @@ pragma solidity ^0.8.12;
 
 contract SocialNetwork { 
     string public name;
+    uint public postCount = 0;
+    mapping(uint => Post) public posts;
+
+    struct Post {
+        uint id;
+        string content;
+        uint tipAmount;
+        address author;
+    }
 
     constructor() public {
         name = "Brink Social Network";
 
+    }
+    function createPost(string memory _content) public {
+        
+        postCount ++;
+        posts[postCount]= Post(postCount, _content, 0, msg.sender);
+       
     }
 }
